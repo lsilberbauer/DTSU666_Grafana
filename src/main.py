eb44cb526f4cff3ea8ffe90abb763e5eb3228a72
Lukas Silberbauer <lukas.silberbauer@taurob.com>
2023-01-29 22:17:12 +0100

    added dashboard, tuned scraping

--- src/main.py
+++ src/main.py
@@ -1,24 +1,25 @@
 import struct
 import time
 
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import Counter, Gauge, start_http_server
 from pymodbus.client import ModbusTcpClient
 
+UA = Gauge('ua', 'Voltage Phase A [V]')
+UB = Gauge('ub', 'Voltage Phase B [V]')
+UC = Gauge('uc', 'Voltage Phase C [V]')
+IA = Gauge('ia', 'Current Phase A [A]')
+IB = Gauge('ib', 'Current Phase B [A]')
+IC = Gauge('ic', 'Current Phase C [A]')
 PT = Gauge('pt', 'Total Power [W]')
+F = Gauge('f', 'Frequency [Hz]')
+IMP = Gauge('impep', 'Imported Power [kWh]')
+EXP = Gauge('expep', 'Exported Power [kWh]')
+ERR = Counter('errors', 'Python Exceptions')
 
-registers = [(0x2006, "Ua", 0.1, "V"),
-             (0x2008, "Ub", 0.1, "V"),
-             (0x200A, "Uc", 0.1, "V"),
-             (0x200C, "Ia", 0.001, "A"),
-             (0x200E, "Ib", 0.001, "A"),
-             (0x2010, "Ic", 0.001, "A"),
-             (0x2012, "Pt", 0.1, "W"),
-             (0x2044, "Freq", 0.01, "Hz"),
-             (0x101E, "ImpEp", 1, "kWh"),
-             (0x1028, "ExpEp", 1, "kWh")]
+client = ModbusTcpClient('waveshare-58a4', 8899)
 
 
-def get_smart_meter_value(client, address):
+def get_smart_meter_value(address):
     result = client.read_holding_registers(address, count=2, slave=1)
     hex_value = result.registers[0] * 0x10000 + result.registers[1]
     if hex_value != 0:
@@ -29,16 +30,24 @@ def get_smart_meter_value(client, address):
 
 if __name__ == "__main__":
     start_http_server(8080)
-
-    client = ModbusTcpClient('waveshare-58a4', 8899)
     client.connect()
 
     try:
         while True:
-            PT.set(get_smart_meter_value(client, 0x2012) * 0.1)
-            print("set PT value")
+            print("polling...")
 
-            time.sleep(3)
+            UA.set(get_smart_meter_value(0x2006) * 0.1)
+            UB.set(get_smart_meter_value(0x2008) * 0.1)
+            UC.set(get_smart_meter_value(0x200A) * 0.1)
+            IA.set(get_smart_meter_value(0x200C) * 0.001)
+            IB.set(get_smart_meter_value(0x200E) * 0.001)
+            IC.set(get_smart_meter_value(0x2010) * 0.001)
+            PT.set(get_smart_meter_value(0x2012) * 0.1)
+            F.set(get_smart_meter_value(0x2044) * 0.01)
+            IMP.set(get_smart_meter_value(0x101E))
+            EXP.set(get_smart_meter_value(0x1028))
+
+            time.sleep(15)
 
     except KeyboardInterrupt as ex:
         client.close()
